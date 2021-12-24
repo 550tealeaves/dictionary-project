7c51d5c63e270cbd1fc8481d15e288c8a39c44fe
nclaire29 <nastasia.claire89@gmail.com>
2021-12-23 23:43:12 -0500

    started image api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,15 +8,24 @@ export default function Dictionary(props){
     let [results, setResults] = useState(null); //3a. create state to track the changes of API results with each word entered - will update pg 
     let [loaded, setLoaded] = useState(false); // tracks whether or not the page was loaded 
     
-    function handleResponse(response){ //Define handleResponse function
+    function handleDictionaryResponse(response){ //Define handleResponse function
         console.log(response.data[0]);
         setResults(response.data[0]);//3b. - every time you get response from dictionary API, update the results and store it 
     }
     
     function search(){ //makes the API call using the url to get the results to a word searched 
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`; //After creating API url, interpolate and add ${keyword} state so any word can be defined
-        axios.get(apiUrl).then(handleResponse); //Use axios.get to make request
+        axios.get(apiUrl).then(handleDictionaryResponse); //Use axios.get to make request
     }
+
+    function handlePexelsResponse(response){
+        console.log(response);
+    }
+    
+    let pexelsApiKey ="563492ad6f91700001000001f6c8ac98c12c4921a81f1c9995975b2b";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+    axios.get(pexelsApiUrl).then(handlePexelsResponse);
+
     
     function handleSubmit(event){
         event.preventDefault();
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Phonetics.css";
+import "./Phonetic.css";
 
 export default function Phonetic(props) {//2. Receives props from Results.js - 3. Create link to API path for audio {props.phonetic.audio} & then add the text path {props.phonetic.text} below
     return (
