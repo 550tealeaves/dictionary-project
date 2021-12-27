cc4d591efcdcd66f536123f06bd3c45fe0ac9c2a
nclaire29 <nastasia.claire89@gmail.com>
2021-12-27 03:36:53 -0500

    Added notes

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,13 +6,13 @@ import "./Dictionary.css";
 
 export default function Dictionary(props){
     let [keyword, setKeyword] = useState(props.defaultKeyword); //create state b/c the value of search entry will change in component
-    let [results, setResults] = useState(null); //3a. create state to track the changes of API results with each word entered - will update pg 
+    let [results, setResults] = useState(null); // create state to track the changes of API results with each word entered - will update pg 
     let [loaded, setLoaded] = useState(false); // tracks whether or not the page was loaded 
     let [photos, setPhotos] = useState(null); //7. tracks the current photo whenever a word is searched for
     
     function handleDictionaryResponse(response){ //Define handleResponse function
         console.log(response.data[0]);
-        setResults(response.data[0]);//3b. - every time you get response from dictionary API, update the results and store it 
+        setResults(response.data[0]);//Every time you get response from dictionary API, update the results and store it 
     }
     
     function search(){ //makes the API call using the url to get the results to a word searched 
@@ -20,13 +20,13 @@ export default function Dictionary(props){
         axios.get(apiUrl).then(handleDictionaryResponse); //Use axios.get to make request
     }
 
-    function handlePexelsResponse(response){
+    function handlePexelsResponse(response){ //4. Function handle houses the setPhotos state - when called will display results in response.data.photos on the pg
         console.log(response);
         setPhotos(response.data.photos); //7a. this is where the photos stored in API results
     }
     
-    let pexelsApiKey ="563492ad6f91700001000001f6c8ac98c12c4921a81f1c9995975b2b";
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let pexelsApiKey ="563492ad6f91700001000001f6c8ac98c12c4921a81f1c9995975b2b"; //3. API key for pexels
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`; //3. Interpolate keword variable - 15. change # of pics shown at the end to 9 (or anything up to 80)
     let headers = { Authorization: `Bearer ${pexelsApiKey}` }; //5. store authorization header in variable - header
     axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse); //lets you authenticate yourself so you get access to API results - replace authorization statement with variable "headers"
 
@@ -46,7 +46,7 @@ export default function Dictionary(props){
     }
 
     if (loaded){ //want dictionary to load a word on default - if loaded, then show the entire form. If not, call the load function which changes setLoaded to true and calls search function
-      return ( //2. Insert component Results - 4. Send property value of {result} as  property, named results to component Results. It will re-render every time the results (setResults) updates 
+      return ( //Insert component Results - Send property value of {result} as  property, named results to component Results. It will re-render every time the results (setResults) updates 
         <div className="Dictionary">
             <section>
                 <h1>Type in a word to look up</h1>
--- src/Photos.js
+++ src/Photos.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./Photos.css";
 
-export default function Photos(props){
-    if (props.photos) {
-        console.log(props.photos);
+export default function Photos(props){//8. Create photos component and sent phots as properties 
+    if (props.photos) { //10. To fix undefined errors, need conditional rendering - IF there are pic results in props.photos, then run a loop through the photos and display them in 3 columns, with each photo clickable to go to the original URL link ELSE return nothing if no results in props.photos  
+        console.log(props.photos); //9. pathway since we are in photos.js is props.propsName - shows results as array
         return (
             <section className="Photos">
                 <div className="row">
-                {props.photos.map(function (photo, index) {
+                {props.photos.map(function (photo, index) { //11. Loop through the array of photos and display them 1 by 1 - display in bootstrap grid and make clickable to original link 
                     return (
                         <div className="col-4" key={index}>
                             <a href={photo.src.original} target="_blank" rel="noreferrer">
