a09fa11afe8b7a3be6cf1ad3fc412014694fbdc8
550tealeaves <nastasia.claire89@gmail.com>
2024-07-14 00:30:04 -0400

    made some edits

--- src/Photos.js
+++ src/Photos.js
@@ -3,7 +3,7 @@ import "./Photos.css";
 
 export default function Photos(props){//8. Create photos component and sent phots as properties 
     if (props.photos) { //10. To fix undefined errors, need conditional rendering - IF there are pic results in props.photos, then run a loop through the photos and display them in 3 columns, with each photo clickable to go to the original URL link ELSE return nothing if no results in props.photos  
-        console.log(props.photos); //9. pathway since we are in photos.js is props.propsName - shows results as array
+        console.log('pics', props.photos); //9. pathway since we are in photos.js is props.propsName - shows results as array
         return (
             <section className="Photos">
                 <div className="row">
